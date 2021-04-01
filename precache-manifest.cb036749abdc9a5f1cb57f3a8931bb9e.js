5801acdf9dfe1c8ac2194267ec1f6f4bef57a588
Veloraine <bivanderr@gmail.com>
2021-04-01 21:24:09 +0700

    initial

---
+++ precache-manifest.cb036749abdc9a5f1cb57f3a8931bb9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf03051e79b79f59847",
+    "url": "/css/app.e797cfb5.css"
+  },
+  {
+    "revision": "171a873e0e38c08935e5",
+    "url": "/css/chunk-vendors.9b9fa459.css"
+  },
+  {
+    "revision": "2e537d03349660bb51851462c06f9c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf03051e79b79f59847",
+    "url": "/js/app.9e0999fb.js"
+  },
+  {
+    "revision": "171a873e0e38c08935e5",
+    "url": "/js/chunk-vendors.6a115e0f.js"
+  },
+  {
+    "revision": "8648399a94b6c0d7028fa04c9c32e07d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
